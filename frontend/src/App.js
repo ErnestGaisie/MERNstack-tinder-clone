81f2e4ac94ce6df423e42764a6b3973e9ae50623
Ernest Gaisie <ernestyawgaisie@gmail.com>
2021-03-25 17:02:26 +0000

    swipe buttons done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import SwipeButtons from "./components/swipebuttons/SwipeButtons";
 import TinderCards from "./components/tindercards/TinderCards";
 function App() {
   return (
     <div className="App">
       <Header />
       <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
